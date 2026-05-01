c6d2d9f2ca19793ac45eb0fb3d39b5253f95fdbd
Simon Merschjohann <smerschjohann@users.noreply.github.com>
2026-05-01 12:36:32 +0000

    fix provision script

--- images/cuda/ROOT/etc/s6-overlay/s6-rc.d/async_provision/prepare.sh
+++ images/cuda/ROOT/etc/s6-overlay/s6-rc.d/async_provision/prepare.sh
@@ -2,11 +2,11 @@
 
 if [ -n "$ASYNC_PROVISION_SCRIPT" ]; then
     echo "Running configured provision script"
-    CURL_OPTS="-kfsSL"
+    CURL_OPTS=(-kfsSL)
     if [ -n "$ASYNC_PROVISION_SCRIPT_TOKEN" ]; then
-        CURL_OPTS="$CURL_OPTS -H 'Authorization: Bearer $ASYNC_PROVISION_SCRIPT_TOKEN'"
+        CURL_OPTS+=(-H "Authorization: Bearer $ASYNC_PROVISION_SCRIPT_TOKEN")
     fi
-    curl $CURL_OPTS "$ASYNC_PROVISION_SCRIPT" -o /tmp/async_provision_script.sh
+    curl "${CURL_OPTS[@]}" "$ASYNC_PROVISION_SCRIPT" -o /tmp/async_provision_script.sh
     chmod +x /tmp/async_provision_script.sh
     /tmp/async_provision_script.sh
 fi
--- images/cuda/ROOT/etc/s6-overlay/s6-rc.d/provision/prepare.sh
+++ images/cuda/ROOT/etc/s6-overlay/s6-rc.d/provision/prepare.sh
@@ -2,11 +2,11 @@
 
 if [ -n "$PROVISION_SCRIPT" ]; then
     echo "Running configured provision script"
-    CURL_OPTS="-kfsSL"
+    CURL_OPTS=(-kfsSL)
     if [ -n "$PROVISION_SCRIPT_TOKEN" ]; then
-        CURL_OPTS="$CURL_OPTS -H 'Authorization: Bearer $PROVISION_SCRIPT_TOKEN'"
+        CURL_OPTS+=(-H "Authorization: Bearer $PROVISION_SCRIPT_TOKEN")
     fi
-    curl $CURL_OPTS "$PROVISION_SCRIPT" -o /tmp/provision_script.sh
+    curl "${CURL_OPTS[@]}" "$PROVISION_SCRIPT" -o /tmp/provision_script.sh
     chmod +x /tmp/provision_script.sh
     /tmp/provision_script.sh
 fi
